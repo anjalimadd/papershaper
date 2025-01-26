dab1a5f96c74a32169249520a401c92ff0e7f118
Harsh Kushwaha <115409050+harshkushwaha7x@users.noreply.github.com>
2025-01-26 23:42:12 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -10,13 +10,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNawjP4NomrvHtsCkCnZCV9zNwr--zpOk",
-  authDomain: "papershaper-88ac3.firebaseapp.com",
-  projectId: "papershaper-88ac3",
-  storageBucket: "papershaper-88ac3.firebasestorage.app",
-  messagingSenderId: "657092655009",
-  appId: "1:657092655009:web:ee8adc8f058005d8313de0",
-  measurementId: "G-XD77D6KZVB",
+  apiKey: "AIzaSyAyDLGBvNv1YjIq0HKuOiGrw-dGXcsl3eo",
+  authDomain: "papershapers-64595.firebaseapp.com",
+  projectId: "papershapers-64595",
+  storageBucket: "papershapers-64595.firebasestorage.app",
+  messagingSenderId: "164202290994",
+  appId: "1:164202290994:web:8c573a00aa28cc47e0412b"
 };
 
 // Initialize Firebase
